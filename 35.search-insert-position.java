aac449b5818983a2faef86feadd5e0319613107a
Guozhi Tang <tangguozhi53@gmail.com>
2020-10-04 00:24:20 -0700

    No.35

--- 35.search-insert-position.java
+++ 35.search-insert-position.java
@@ -51,8 +51,39 @@
 
 // @lc code=start
 class Solution {
+
+    // Time: O(logn)
+    // Space: O(1)
+    // BinarySearch
     public int searchInsert(int[] nums, int target) {
-        
+        // Corner cases
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        // Then the start and end will be the two which are the closest to the target
+        // If target is still smaller than the left
+        if (nums[start] >= target) {
+            return start;
+        }
+        // If the target is smaller than the right
+        else if (nums[end] >= target) {
+            return end;
+        } else {
+            return end + 1;
+        }
     }
 }
 // @lc code=end
