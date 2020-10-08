62defe4d128e345cde90db3125580e06ba71bd83
Guozhi Tang <tangguozhi53@gmail.com>
2020-10-07 21:58:52 -0700

    No. 153 154

--- 153.find-minimum-in-rotated-sorted-array.java
+++ 153.find-minimum-in-rotated-sorted-array.java
@@ -40,24 +40,55 @@
 
 // @lc code=start
 class Solution {
-    // Binary Search
+
+        
     // Time: O(logn)
     // Space: O(1)
+    // Binary Search
     public int findMin(int[] nums) {
-        // Corner Cases
-        if (nums == null || nums.length == 0) return -1;
+        // Corner cases
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
 
-        int begin = 0, end = nums.length - 1;
-        while (begin <= end) {
-            int mid = begin + (end - begin) / 2;
-            // if the right part is ascending, then min should be at the left
-            // but the leftmost could be the min
-            if (nums[mid] < nums[end]) end = mid;
-            // else the min should be at the right
-            else begin = mid + 1;
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < nums[end]) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+
+        // Judge which one is smaller
+        if (nums[start] < nums[end]) {
+            return nums[start];
         }
         return nums[end];
     }
+
+
+
+
+    // // Binary Search
+    // // Time: O(logn)
+    // // Space: O(1)
+    // public int findMin(int[] nums) {
+    //     // Corner Cases
+    //     if (nums == null || nums.length == 0) return -1;
+
+    //     int begin = 0, end = nums.length - 1;
+    //     while (begin <= end) {
+    //         int mid = begin + (end - begin) / 2;
+    //         // if the right part is ascending, then min should be at the left
+    //         // but the leftmost could be the min
+    //         if (nums[mid] < nums[end]) end = mid;
+    //         // else the min should be at the right
+    //         else begin = mid + 1;
+    //     }
+    //     return nums[end];
+    // }
 }
 // @lc code=end
 
--- 154.find-minimum-in-rotated-sorted-array-ii.java
+++ 154.find-minimum-in-rotated-sorted-array-ii.java
@@ -45,22 +45,52 @@
 
 // @lc code=start
 class Solution {
-    // Binary Search
+
     // Time: O(logn)
     // Space: O(1)
+    // Binary Search
     public int findMin(int[] nums) {
-        // Corner Cases
-        if (nums == null || nums.length == 0) return -1;
-
-        int begin = 0, end = nums.length - 1;
-        while (begin < end) {
-            int mid = begin + (end - begin) / 2;
-            if (nums[mid] == nums[end]) end--;
-            else if (nums[mid] < nums[end]) end = mid;
-            else begin = mid + 1;
+        // Corner cases
+        if (nums == null || nums.length == 0) {
+            return -1;
         }
-        return nums[begin];
+
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < nums[end]) {
+                end = mid;
+            } else if (nums[mid] > nums[end]){
+                start = mid;
+            } else {
+                // Why here should use end to judge not the begin
+                // Maybe because here's to ask for the minimum?
+                end--;
+            }
+        }
+
+        if (nums[start] < nums[end]) {
+            return nums[start];
+        }
+        return nums[end];
     }
+
+    // // Binary Search
+    // // Time: O(logn)
+    // // Space: O(1)
+    // public int findMin(int[] nums) {
+    //     // Corner Cases
+    //     if (nums == null || nums.length == 0) return -1;
+
+    //     int begin = 0, end = nums.length - 1;
+    //     while (begin < end) {
+    //         int mid = begin + (end - begin) / 2;
+    //         if (nums[mid] == nums[end]) end--;
+    //         else if (nums[mid] < nums[end]) end = mid;
+    //         else begin = mid + 1;
+    //     }
+    //     return nums[begin];
+    // }
 }
 // @lc code=end
 
