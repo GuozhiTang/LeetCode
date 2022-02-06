0d64c1ab399ee013880e14a9c966ee6b590fb07f
Guozhi Tang <tangguozhi53@gmail.com>
2022-02-05 17:48:40 -0800

    No.35

--- 35.search-insert-position.java
+++ 35.search-insert-position.java
@@ -52,11 +52,12 @@
 // @lc code=start
 class Solution {
 
+
     // Time: O(logn)
-    // Space: O(1)
-    // BinarySearch
+    // Space: O(n)
+    // Binary Search
     public int searchInsert(int[] nums, int target) {
-        // Corner cases
+        // Corner Cases
         if (nums == null || nums.length == 0) {
             return -1;
         }
@@ -77,14 +78,48 @@ class Solution {
         // If target is still smaller than the left
         if (nums[start] >= target) {
             return start;
-        }
         // If the target is smaller than the right
-        else if (nums[end] >= target) {
+        } else if (nums[end] >= target) {
             return end;
         } else {
             return end + 1;
         }
     }
+
+
+    // // Time: O(logn)
+    // // Space: O(1)
+    // // BinarySearch
+    // public int searchInsert(int[] nums, int target) {
+    //     // Corner cases
+    //     if (nums == null || nums.length == 0) {
+    //         return -1;
+    //     }
+
+    //     int start = 0, end = nums.length - 1;
+    //     while (start + 1 < end) {
+    //         int mid = start + (end - start) / 2;
+    //         if (nums[mid] == target) {
+    //             return mid;
+    //         } else if (nums[mid] < target) {
+    //             start = mid;
+    //         } else {
+    //             end = mid;
+    //         }
+    //     }
+
+    //     // Then the start and end will be the two which are the closest to the target
+    //     // If target is still smaller than the left
+    //     if (nums[start] >= target) {
+    //         return start;
+    //     }
+    //     // If the target is smaller than the right
+    //     else if (nums[end] >= target) {
+    //         return end;
+    //     } else {
+    //         return end + 1;
+    //     }
+    // }
 }
 // @lc code=end
 
