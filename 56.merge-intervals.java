32ba268fac733ee24420a08cd1b2cd6535ed4005
Guozhi Tang <tangguozhi53@gmail.com>
2022-02-17 02:46:01 -0800

    02/16/2022: No.252, No.253, updated No.56

--- 56.merge-intervals.java
+++ 56.merge-intervals.java
@@ -58,30 +58,62 @@ class Solution {
     // Comparator + Iteration
     // https://leetcode.com/problems/merge-intervals/discuss/21222/A-simple-Java-solution
     public int[][] merge(int[][] intervals) {
-       // Corner Cases
-       if (intervals.length == 0) {
-           return new int[][] {};
-       }
+        // Corner Cases
+        if (intervals.length == 0) {
+            return new int[][] {};
+        }
+ 
+        // O(nlogn)
+        Arrays.sort(intervals, (a,b) -> (a[0] - b[0]));
+ 
+        List<int[]> result = new ArrayList<int[]>();
+        int start = intervals[0][0], end = intervals[0][1];
 
-       // O(nlogn)
-       Arrays.sort(intervals, (a,b) -> (a[0] - b[0]));
+        for (int[] interval : intervals) {
+            if (end >= interval[0]) { // when overlapping
+                end = Math.max(end, interval[1]);
+            } else { // when disjoint
+                result.add(new int[] {start, end});
+                start = interval[0];
+                end = interval[1];
+            }
+        }
+        result.add(new int[] {start, end});
 
-       List<int[]> result = new ArrayList<int[]>();
-       int[] newInterval = intervals[0];
+        return result.toArray(new int[result.size()][]);
+     }
 
-       // add `new` to the result first and update the new[1] based on if condition
-       result.add(newInterval);
-       for (int[] interval : intervals) {
-           if (interval[0] <= newInterval[1]) {
-               newInterval[1] = Math.max(newInterval[1], interval[1]);
-           } else { // Once meet disjoint intervals, keep previous `new` and add new `new` to the result waiting for updates.
-               newInterval = interval;
-               result.add(newInterval);
-           }
-       }
+    /*******************************************************/
 
-       return result.toArray(new int[result.size()][]);
-    }
+    // // Time: O(nlogn)
+    // // Space: O(n)
+    // // Comparator + Iteration
+    // // https://leetcode.com/problems/merge-intervals/discuss/21222/A-simple-Java-solution
+    // public int[][] merge(int[][] intervals) {
+    //    // Corner Cases
+    //    if (intervals.length == 0) {
+    //        return new int[][] {};
+    //    }
+
+    //    // O(nlogn)
+    //    Arrays.sort(intervals, (a,b) -> (a[0] - b[0]));
+
+    //    List<int[]> result = new ArrayList<int[]>();
+    //    int[] newInterval = intervals[0];
+
+    //    // add `new` to the result first and update the new[1] based on if condition
+    //    result.add(newInterval);
+    //    for (int[] interval : intervals) {
+    //        if (interval[0] <= newInterval[1]) {
+    //            newInterval[1] = Math.max(newInterval[1], interval[1]);
+    //        } else { // Once meet disjoint intervals, keep previous `new` and add new `new` to the result waiting for updates.
+    //            newInterval = interval;
+    //            result.add(newInterval);
+    //        }
+    //    }
+
+    //    return result.toArray(new int[result.size()][]);
+    // }
 }
 // @lc code=end
 
